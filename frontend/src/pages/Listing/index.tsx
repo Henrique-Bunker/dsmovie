6f49e132e109fb42f5432be1f67f95bc2f1ea421
Henrique Souza <henrique.asouza@outlook.com>
2022-01-14 23:08:05 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -25,9 +25,13 @@ const Listing = () => {
       .then((response) => setPage(response.data));
   }, [pageNumber]);
 
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  };
+
   return (
     <>
-      <Pagination />;
+      <Pagination page={page} onChange={handlePageChange} />;
       <div className="container">
         <div className="row">
           {page.content.map((movie) => (
